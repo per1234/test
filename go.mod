a48b6f577d5c069e8e9be3a047fd2d767fad59ab
per1234 <accounts@perglass.com>
2024-08-11 07:24:16 -0700

    Set up for testing codecov

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/per1234/test
+
+go 1.21.0
+
+toolchain go1.21.13
+
+require (
+	github.com/go-task/task/v3 v3.38.0
+)
---
+++ tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+
+// Package tools defines the project's Go module tool dependencies.
+// See: https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+package tools
+
+import (
+	_ "github.com/editorconfig-checker/editorconfig-checker/v3/cmd/editorconfig-checker"
+	_ "github.com/go-task/task/v3/cmd/task"
+)
